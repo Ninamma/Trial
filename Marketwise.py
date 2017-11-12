c41c74bb67e160b5f745b7a9e885e17917082e8a
Nina Rai <nina_rai@hotmail.co.uk>
2017-11-12 16:37:58 +0000

    changes

--- Marketwise.py
+++ Marketwise.py
@@ -14,28 +14,28 @@ def home():
 #def news():
 #    return render_template("newsletter.html")
 
-@app.route("/northamerica")
-def northamerica():
-    return render_template("northamerica.html", entries = Adapter.final_adapter('assets AND "north america"'))
+#@app.route("/northamerica")
+#def northamerica():
+#    return render_template("northamerica.html", entries = Adapter.final_adapter('assets AND "north america"'))
 
-@app.route("/southamerica")
-def southamerica():
-    return render_template("southamerica.html", entries = Adapter.final_adapter('assets AND "south america"'))
+#@app.route("/southamerica")
+#def southamerica():
+#    return render_template("southamerica.html", entries = Adapter.final_adapter('assets AND "south america"'))
 
-@app.route("/europe")
-def europe():
-    return render_template("europe.html", entries = Adapter.final_adapter('assets AND europe'))
+#@app.route("/europe")
+#def europe():
+#    return render_template("europe.html", entries = Adapter.final_adapter('assets AND europe'))
 
-@app.route("/africa")
-def africa():
-    return render_template("africa.html", entries = Adapter.final_adapter('assets AND africa'))
+#@app.route("/africa")
+#def africa():
+#    return render_template("africa.html", entries = Adapter.final_adapter('assets AND africa'))
 
-@app.route("/asia")
-def asia():
-    return render_template("asia.html", entries = Adapter.final_adapter('assets AND asia'))
+#@app.route("/asia")
+#def asia():
+#    return render_template("asia.html", entries = Adapter.final_adapter('assets AND asia'))
 
-@app.route("/australia")
-def australia():
-    return render_template("australia.html", entries = Adapter.final_adapter('assets AND australia'))
+#@app.route("/australia")
+#def australia():
+#    return render_template("australia.html", entries = Adapter.final_adapter('assets AND australia'))
 
 app.run(debug = True)
