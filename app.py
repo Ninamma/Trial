b06a05a88879b9c1e8b183fd1bfaafa4f0a1df7c
Nina Rai <nina_rai@hotmail.co.uk>
2017-11-12 17:37:02 +0000

    slkjf

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template#, request
+#from APIs import Adapter
 app = Flask("MyApp")
 
 @app.route("/")
 def home():
-    return '<h1>will this finaly work??</h1>'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
